2566a97567e08e94d7c0bae2c6befbc6a43e85b7
imisak <ivan.misak@gmail.com>
2019-05-05 19:14:23 +0200

    pagination error

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,12 +1,34 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Pagination } from "./Pagination";
 
 export class Posts extends Component {
   state = {
-    posts: undefined
+    posts: undefined,
+    totalCount: 0,
+    currentPage: 1,
+    itemsPerPage: 5
+  };
+
+  setPage = event => {
+    const { value } = event.target;
+    this.setState({
+      currentPage: parseInt(value)
+    });
+  };
+
+  setTotalCount = posts => {
+    this.setState({
+      totalCount: posts.length
+    });
   };
 
   componentDidMount() {
+    const { posts } = this.props;
+    if (posts) {
+      this.setTotalCount(posts);
+    }
+
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then(response => response.json())
       .then(posts =>
@@ -17,8 +39,28 @@ export class Posts extends Component {
       .catch(error => console.log(error));
   }
 
+  componentWillUpdate(nextProps) {
+    const { posts } = this.props;
+
+    if (posts !== nextProps.posts) {
+      this.setTotalCount(nextProps.posts);
+    }
+  }
+
+  renderItem = ({ id, title }) => (
+    <div key={id}>
+      <Link to={`/posts/${id}`}>{title}</Link>
+    </div>
+  );
+
   render() {
     const { posts } = this.state;
+
+    const { itemsPerPage, currentPage } = this.state;
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const pagedTodos = posts.slice(startIndex, endIndex);
+
     return (
       <div className="wrapper">
         <h2>POSTS</h2>
@@ -39,6 +81,9 @@ export class Posts extends Component {
         ) : (
           <div className="loading">Loading...</div>
         )}
+
+        {pagedTodos.map(item => this.renderItem(item))}
+        <Pagination {...this.state} setPage={this.setPage} />
       </div>
     );
   }
